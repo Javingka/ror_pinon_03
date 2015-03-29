9ec4ea6dc8ea57b34f61e2f6927abf611847aa59
Javier Cruz <jcruzsm@gmail.com>
2015-03-29 15:58:44 -0300

    Finish statis pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,12 +3,19 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
-    assert_response :success
+    assert_response :success # The response :success is an abstract representation of the underlying HTTP status code (in this case, 200 OK).
+		assert_select "title", "Home | Piñon"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+		assert_select "title", "Help | Piñon"
   end
 
+	test "should get about" do
+		get :about
+		assert_response :success
+		assert_select "title", "About | Piñon"
+	end
 end
