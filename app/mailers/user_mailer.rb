104b447afb95ecd03a626fed6f801b9e929fbec5
Javier Cruz <jcruzsm@gmail.com>
2015-04-18 21:13:28 -0300

    Add password resets & email configuration using sendgrid

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "Activación de cuenta Piñon"
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Nuevo password "
   end
 end
