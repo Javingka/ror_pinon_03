448ca36e42411b214bb9448100136dcf2d651c33
Javier Cruz <jcruzsm@gmail.com>
2015-05-09 13:20:27 -0300

    included facebook-share again

---
+++ config/initializers/facebook_share.rb
@@ -0,0 +1,12 @@
+FacebookShare.default_facebook_share_options = {
+  :framework => :jquery,
+  :jquery_function => "$",
+
+  :app_id => "1432549193716347",
+  :status => "false",
+  :cookie => "false",
+  :xfbml => "false",
+
+  :selector => '.fb_share',
+  :locale => "en_US"
+}
