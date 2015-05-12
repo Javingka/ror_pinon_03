7c51d69e8ce03ca37e454cb6a42e16fbab90c360
Javier Cruz <jcruzsm@gmail.com>
2015-05-12 03:09:07 -0300

    actualizado mail para colocar lista de motos piñon

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,7 @@ class StaticPagesController < ApplicationController
       @micropost = current_user.microposts.build 
       @feed_items = current_user.feed.paginate(page: params[:page],:per_page => 10) # feed declared in user model.
     end
-    @userPinon = User.find_by(email: 'admin@pinon.cl') #userPinon para mostrar las motos piñon
+    @userPinon = User.find_by(email: 'contacto@tallerpinon.cl') #userPinon para mostrar las motos piñon
     if !@userPinon.nil?  # I had to put this in order to pass the test 
       @motos = @userPinon.motos.paginate(page: params[:page],  :per_page => 2) 
     end
