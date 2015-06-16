8e79149e7da62428c2de14d1790fb78b8763b2bd
Javier Cruz <jcruzsm@gmail.com>
2015-06-15 23:23:14 -0300

    new users follow Taller Piñon

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -6,6 +6,12 @@ class AccountActivationsController < ApplicationController
       user.activate
       log_in user
       flash[:success] = "Cuenta activada!"
+      #When the acount is activated, automaticaly all users follow Taller Piñon
+      r = Relationship.new
+      r.follower_id = current_user.id 
+      r.followed_id = 1 #Taller Piñon user number is 1
+      r.save
+
       redirect_to root_url.sub('pinon.herokuapp.com', 'www.tallerpinon.cl')# user(subdomain: 'www.tallerdinamo.cl') # redirect_back_or user
     else
       flash[:danger] = "Link de activación invalido"
